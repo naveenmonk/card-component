42c3cb4b7f5a069f7ea96f1cde5a29ed3bb805d3
NaveenMONK <naveenbalaji786@gmail.com>
2022-03-08 09:59:42 +0530

    All done

---
+++ src/components/card-list/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Card from "../card"
+
+import '../card-list/card-list.css';
+
+const CardList = ({ users }) => {
+    return (
+      <div className="card-list">
+        {users.length > 0 ? (
+          users.map((user) => (
+            <Card
+              key={user.id}
+              id={user.id}
+              name={user.name}
+              email={user.email}
+            />
+          ))
+        ) : (
+          <h1 style={{ textAlign: 'center' }}>No Users Found !!!</h1>
+        )}
+      </div>
+    );
+  };
+
+export default CardList;
